60984bd3dad695019104c600533ef4b726a29ee6
dipto230 <diptoroy230305@gmail.com>
2026-02-11 18:23:21 +0530

    working on admin section

--- src/components/educator/Navbar.jsx
+++ src/components/educator/Navbar.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { assets, dummyEducatorData } from '../../assets/assets'
+import { UserButton, useUser } from '@clerk/clerk-react'
+import { Link } from 'react-router-dom'
+
 
 const Navbar = () => {
+  const educatorData = dummyEducatorData
+  const {user} =  useUser()
   return (
-    <div> Edu Navbar</div>
+    <div className='h-16 flex items-center justify-between px-6 bg-white border-b border-gray-200 shadow-sm'>
+      <Link to="/">
+        <img src={assets.logo_mang} alt='logo' className='h-8 w-auto object-contain'
+        
+        />
+
+      </Link>
+      <div className='flex items-center gap-5 text-gray-500 relative'>
+        <p>Hi ! {user ? user.fullName : 'Developer'}</p>
+        {user ? <UserButton /> : <img className='max-w-8' src={assets.profile_img } />}
+
+      </div>
+    </div>
   )
 }
 
--- src/pages/educator/Educator.jsx
+++ src/pages/educator/Educator.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../../components/educator/Navbar'
+import Sidebar from '../../components/educator/Sidebar'
+import Footer from '../../components/educator/Footer'
 
 const Educator = () => {
   return (
-      <div>
-          <h1>Educator page</h1>
-          <div>
-              {<Outlet/>}
-          </div>
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      
+      <Navbar />
+
+      <div className="flex flex-1">
+        <Sidebar />
+
+        <main className="flex-1 p-6">
+          <Outlet />
+        </main>
+      </div>
+
+      <Footer />
+
     </div>
   )
 }
+
 
 export default Educator
